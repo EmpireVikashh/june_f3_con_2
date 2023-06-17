c03948dbe476e9eeb6bc171bcb062e5e2f21b94b
EmpireVikashh <116377338+EmpireVikashh@users.noreply.github.com>
2023-06-17 23:19:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+let employeesArr = [];
+let id = 1;
+let messageSection = document.getElementsByClassName("message-section")[0];
+
+// -------for adding new user----------------------------
+function addUser() {
+  let inputs = document.getElementsByTagName("input");
+  let name = inputs[0].value;
+  let profession = inputs[1].value;
+  let age = inputs[2].value;
+
+  inputs[0].value = "";
+  inputs[1].value = "";
+  inputs[2].value = "";
+
+  if (name && profession && age) {
+    let ob = {
+      id: id++,
+      name: name,
+      profession: profession,
+      age: age,
+    };
+    employeesArr.push(ob);
+    success();
+
+    updateEmployeeList();
+  } else {
+    failed();
+  }
+}
+
+// -----if user doesnt fill all field, it will show error message---
+function failed() {
+  let message = document.createElement("p");
+  message.innerText =
+    "Error : Please Make sure All the fields are filled before adding in an employee !";
+  message.className = "error";
+  messageSection.innerHTML = "";
+  messageSection.append(message);
+
+
+}
+
+// ---if the employee data is successfully added !--------
+function success() {
+  let message = document.createElement("p");
+  message.innerText = "Success : Employee Added!";
+  message.className = "success";
+  messageSection.innerHTML = "";
+  messageSection.append(message);
+
+}
+
+// ------for updating the listof Employees------------------
+function updateEmployeeList() {
+  let employees = document.getElementsByClassName("employees-info")[0];
+
+  employees.innerHTML = "";
+  employeesArr.forEach((employee) => {
+    employees.innerHTML += `
+       <div class="employee-details" id="user-${employee.id}">
+        <section class="details">
+        <span>ID: ${employee.id}, &nbsp;</span>
+        <span >Name: ${employee.name}, &nbsp;</span>
+        <span>Profession: ${employee.profession}, &nbsp;</span>
+        <span>Age:${employee.age} &nbsp;</span>
+    </section>
+    <button class="delete-user" onclick="deleteUser(event)">Delete User</button>
+    </div> 
+        `;
+  });
+}
+
+// ------------for deleting User-----------------------
+function deleteUser(event) {
+  let deleteBtn = event.target;
+//   console.log(deleteBtn);
+  let user = deleteBtn.parentElement;
+//   console.log(user);
+  let userId = parseInt(user.id.split("-")[1]);
+//   console.log(user.id.split("-")[0]);
+  user.remove();
+//   let removeIndex = -1;
+
+  for (let i = 0; i < employeesArr.length; i++) {
+    if (employeesArr[i].id === userId) {
+      employeesArr.splice(i, 1);
+    //   removeIndex = i;
+    }
+  }
+
+  updateEmployeeList();
+}
